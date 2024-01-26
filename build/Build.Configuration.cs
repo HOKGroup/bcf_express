b0e1a79ea8e963ae89e79768bc86f1b7bd00eedf
Dan Siroky <dan.siroky@hok.com>
2024-01-26 10:39:11 -0600

    Add build project
    Thanks to https://github.com/jeremytammik/RevitLookup/

---
+++ build/Build.Configuration.cs
@@ -0,0 +1,31 @@
+﻿using Nuke.Common.IO;
+
+sealed partial class Build
+{
+    readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
+    readonly AbsolutePath ChangeLogPath = RootDirectory / "Changelog.md";
+
+    protected override void OnBuildInitialized()
+    {
+        Configurations =
+        [
+            "Release*",
+            "Installer*"
+        ];
+
+        InstallersMap = new()
+        {
+            {Solution.Installer, Solution.BCFExpress}
+        };
+
+        VersionMap = new()
+        {
+            {"Release R19", "2019.0.01"},
+            {"Release R20", "2020.0.01"},
+            {"Release R21", "2021.0.01"},
+            {"Release R22", "2022.0.01"},
+            {"Release R23", "2023.0.01"},
+            {"Release R24", "2024.0.01"}
+        };
+    }
+}
